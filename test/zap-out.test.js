012bc76dfbfb7aa00bd494cc829b3099db3c60b7
nightwing-yb <nightwing-of-the-bay@protonmail.com>
2021-12-09 02:49:47 +0530

    boilerplate setup for ZapOut tests

--- test/zap-out.test.js
+++ test/zap-out.test.js
@@ -19,76 +19,121 @@ describe("ZapOutV1 Test", function () {
   beforeEach(async function () {
     signers = await ethers.getSigners();
 
+    // Deploy ZapOut contract.
     const ZapOutFactory = await ethers.getContractFactory("ZapOutV1");
     ZapOut = await ZapOutFactory.deploy(ROUTER, WMOVR);
     await ZapOut.deployed();
 
-    // Zap-In contract to add liquidity easily for tests.
+    // Deploy ZapIn contract; To add liquidity easily for testing zapping-out.
     const ZapInFactory = await ethers.getContractFactory("ZapInV1");
     ZapIn = await ZapInFactory.deploy(ROUTER, FACTORY, WMOVR);
     await ZapIn.deployed();
 
+    // SolarBeam DEX contracts
     router = new ethers.Contract(ROUTER, SolarRouter.abi, ethers.provider);
     factory = new ethers.Contract(FACTORY, SolarFactory.abi, ethers.provider);
-    console.log("--------");
+    console.log("---- CONTRACT SETUP COMPLETE ----");
   });
 
-  it("ZapOut from WMOVR<>FRAX to MOVR", async () => {
-    // Add liquidity worth 5 MOVR to WMOVR<>FRAX pool.
-    const signer = signers[0];
-    const PAIR_ADDRESS = await factory.getPair(WMOVR, FRAX);
-    const LP_PAIR = makePair(PAIR_ADDRESS);
+  describe("Zap out from WMOVR<>FRAX", function () {
+    let signer;
+    let PAIR_ADDRESS;
+    let LP_PAIR;
+    let movrToLP;
+    let LPBought;
+    beforeEach(async function () {
+      // ADD 10 $MOVR WORTH LIQUIDITY TO POOL
 
-    const movrToInvest = ethers.BigNumber.from(ethers.utils.parseEther("5"));
+      signer = signers[0];
+      PAIR_ADDRESS = await factory.getPair(WMOVR, FRAX);
+      LP_PAIR = makePair(PAIR_ADDRESS);
+      movrToLP = ethers.BigNumber.from(ethers.utils.parseEther("10"));
 
-    const amount0 = await getAmountsOut(router, movrToInvest.div(2), [
-      WMOVR,
-      FRAX,
-    ]);
-    let movrBalance = await ethers.provider.getBalance(signer.address);
-    console.log(
-      "MOVR balance before Zap:",
-      bnDiv1e18(movrBalance).toString(),
-      "$MOVR"
-    );
-    const amount1 = movrToInvest.div(2);
-    const minLP = await calculateMinimumLP(LP_PAIR, amount0, amount1, 5);
-    let LPbalance = await LP_PAIR.balanceOf(signer.address);
+      const amount0 = await getAmountsOut(router, movrToLP.div(2), [
+        WMOVR,
+        FRAX,
+      ]);
+      const amount1 = movrToLP.div(2);
+      const minLP = await calculateMinimumLP(LP_PAIR, amount0, amount1, 1);
 
-    // zapIn(address fromToken, address toPool, uint256 amountToZap, uint256 minimumLPBought)
+      let lpBalance = await LP_PAIR.balanceOf(signer.address);
 
-    const tx = await ZapIn.zapIn(
-      ethers.constants.AddressZero,
-      PAIR_ADDRESS,
-      movrToInvest,
-      minLP,
-      { value: movrToInvest }
-    );
+      await ZapIn.zapIn(
+        ethers.constants.AddressZero,
+        PAIR_ADDRESS,
+        movrToLP,
+        minLP,
+        { value: movrToLP }
+      );
 
-    const LPBought = (await LP_PAIR.balanceOf(signer.address)).sub(LPbalance);
-    console.log("LP Bought", LPBought.toString());
-    movrBalance = await ethers.provider.getBalance(signer.address);
-    console.log(
-      "MOVR balance after zap-in:",
-      bnDiv1e18(movrBalance).toString(),
-      "$MOVR"
-    );
-    console.log("--- Zapping out ---");
-    await LP_PAIR.connect(signer).approve(ZapOut.address, LPBought);
-    // zapOut(address fromLP, address to, uint256 lpAmount, address[] memory path0, address[] memory path1)
-    const zapOutTx = await ZapOut.zapOut(
-      PAIR_ADDRESS,
-      ethers.constants.AddressZero,
-      LPBought,
-      [FRAX, WMOVR],
-      []
-    );
+      LPBought = (await LP_PAIR.balanceOf(signer.address)).sub(lpBalance);
+      console.log("---- LP ADDED ----");
+    });
 
-    movrBalance = await ethers.provider.getBalance(signer.address);
-    console.log(
-      "MOVR balance after zap-out:",
-      bnDiv1e18(movrBalance).toString(),
-      "$MOVR"
-    );
+    it("1.", async () => {
+      console.log(LPBought.toString());
+    });
+    it("2.", async () => {
+      console.log(LPBought.toString());
+    });
   });
+
+  // it("ZapOut from WMOVR<>FRAX to MOVR", async () => {
+  //   // Add liquidity worth 5 MOVR to WMOVR<>FRAX pool.
+  //   const signer = signers[0];
+  //   const PAIR_ADDRESS = await factory.getPair(WMOVR, FRAX);
+  //   const LP_PAIR = makePair(PAIR_ADDRESS);
+
+  //   const movrToInvest = ethers.BigNumber.from(ethers.utils.parseEther("5"));
+
+  //   const amount0 = await getAmountsOut(router, movrToInvest.div(2), [
+  //     WMOVR,
+  //     FRAX,
+  //   ]);
+  //   let movrBalance = await ethers.provider.getBalance(signer.address);
+  //   console.log(
+  //     "MOVR balance before Zap:",
+  //     bnDiv1e18(movrBalance).toString(),
+  //     "$MOVR"
+  //   );
+  //   const amount1 = movrToInvest.div(2);
+  //   const minLP = await calculateMinimumLP(LP_PAIR, amount0, amount1, 5);
+  //   let LPbalance = await LP_PAIR.balanceOf(signer.address);
+
+  //   // zapIn(address fromToken, address toPool, uint256 amountToZap, uint256 minimumLPBought)
+
+  //   const tx = await ZapIn.zapIn(
+  //     ethers.constants.AddressZero,
+  //     PAIR_ADDRESS,
+  //     movrToInvest,
+  //     minLP,
+  //     { value: movrToInvest }
+  //   );
+
+  //   const LPBought = (await LP_PAIR.balanceOf(signer.address)).sub(LPbalance);
+  //   console.log("LP Bought", LPBought.toString());
+  //   movrBalance = await ethers.provider.getBalance(signer.address);
+  //   console.log(
+  //     "MOVR balance after zap-in:",
+  //     bnDiv1e18(movrBalance).toString(),
+  //     "$MOVR"
+  //   );
+  //   console.log("--- Zapping out ---");
+  //   await LP_PAIR.connect(signer).approve(ZapOut.address, LPBought);
+  //   // zapOut(address fromLP, address to, uint256 lpAmount, address[] memory path0, address[] memory path1)
+  //   const zapOutTx = await ZapOut.zapOut(
+  //     PAIR_ADDRESS,
+  //     ethers.constants.AddressZero,
+  //     LPBought,
+  //     [FRAX, WMOVR],
+  //     []
+  //   );
+
+  //   movrBalance = await ethers.provider.getBalance(signer.address);
+  //   console.log(
+  //     "MOVR balance after zap-out:",
+  //     bnDiv1e18(movrBalance).toString(),
+  //     "$MOVR"
+  //   );
+  // });
 });
